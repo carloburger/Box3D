b56025c211fa02eeb9888e806dec070aaedf6c00
Katelyn Wong <katelynwong100@gmail.com>
2026-05-13 12:46:03 -0400

    tryna make the bed open when you click on it lol

--- Assets/Level One Assets/BedScript.cs
+++ Assets/Level One Assets/BedScript.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class BedScript : MonoBehaviour
 {
+    public Vector3 upright;
+    public Vector3 uprightPos;
+    public Vector3 open;
+    public Vector3 openPos;
+    private bool isUpright = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        upright = transform.eulerAngles;
+        uprightPos = transform.position;
     }
 
     // Update is called once per frame
@@ -20,5 +26,21 @@ public class BedScript : MonoBehaviour
         {
             // I don't know, pick up the object? 
         }
+    }
+
+    void OnMouseDown()
+    {
+        if (isUpright)
+        {
+            transform.eulerAngles = open;
+            transform.position = openPos;
+        }
+        else
+        {
+            transform.eulerAngles = upright;
+            transform.position = uprightPos;
+        }
+        isUpright = !isUpright;
+      
     }
 }
