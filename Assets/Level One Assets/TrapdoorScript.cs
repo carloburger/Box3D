13aae29df03378c1994ed0f4f18cf620871f3e67
Katelyn Wong <katelynwong100@gmail.com>
2026-05-14 18:06:30 -0400

    now you can go up and down the trapdoor!

--- Assets/Level One Assets/TrapdoorScript.cs
+++ Assets/Level One Assets/TrapdoorScript.cs
@@ -6,9 +6,11 @@ public class TrapdoorScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject Camera;
     public Vector3 CamPos;
+
+    public float drop;
     void Start()
     {
-        
+        CamPos = Camera.transform.position;
     }
 
     // Update is called once per frame
@@ -22,7 +24,15 @@ public class TrapdoorScript : MonoBehaviour
         if (gameObject.activeSelf)
         {
             CamPos = Camera.transform.position;
-            Camera.transform.position = new Vector3(CamPos.x, CamPos.y - 4, CamPos.z);
+            if (CamPos.y > drop)
+            {
+                Camera.transform.position = new Vector3(CamPos.x, CamPos.y - 4, CamPos.z);
+            }
+            else
+            {
+                Camera.transform.position = new Vector3(CamPos.x, CamPos.y + 4, CamPos.z);
+            }
         }
+        
     }
 }
